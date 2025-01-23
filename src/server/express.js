8f8173d52a6455e518da48dd335024538ee3e598
lack <valentin.dautrement@protonmail.com>
2025-01-23 15:08:17 +0100

    bugfix(views): added public as a static path express server

--- src/server/express.js
+++ src/server/express.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 function setupExpressServer() {
     const app = express();
+
+    app.use(express.static('../../public'));
     
     app.set('view engine', 'ejs');
     app.use(express.static('public'));
